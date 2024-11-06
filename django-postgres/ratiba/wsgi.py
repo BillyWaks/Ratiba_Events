b3fc21a244cd1905dcb1ca1b8d83071bcb3d6360
“BillyWaks” <136112711+BillyWaks@users.noreply.github.com>
2024-11-06 18:21:48 +0300

    changes

--- django-postgres/ratiba/wsgi.py
+++ django-postgres/ratiba/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ratiba.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-postgres.ratiba.settings')
 
 application = get_wsgi_application()
