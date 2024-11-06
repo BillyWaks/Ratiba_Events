53a40a288546e35d94bf050ccc6f3ec40e5f2f3b
“BillyWaks” <136112711+BillyWaks@users.noreply.github.com>
2024-11-06 19:49:31 +0300

    changed Debug to False

--- django-postgres/ratiba/settings.py
+++ django-postgres/ratiba/settings.py
@@ -35,27 +35,27 @@ SECRET_KEY = env("SECRET_KEY")
 # The `DYNO` env var is set on Heroku CI, but it's not a real Heroku app, so we have to
 # also explicitly exclude CI:
 # https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
-IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
+# IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
-# SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
-    DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# if not IS_HEROKU_APP:
+#     DEBUG = True
 
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # validation of the Host header in the incoming HTTP request. On other platforms you may need to
 # list the expected hostnames explicitly in production to prevent HTTP Host header attacks. See:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#std-setting-ALLOWED_HOSTS
-if IS_HEROKU_APP:
-    ALLOWED_HOSTS = ["*"]
-else:
-    ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+# if IS_HEROKU_APP:
+#     ALLOWED_HOSTS = ["*"]
+# else:
+#     ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
 
 
 
 # # Security settings
-# DEBUG = False 
+DEBUG = False 
 # #env('DEBUG') #== 'False'  # Ensure this is set to 'False' in production
-# ALLOWED_HOSTS = ['djangoratiba.herokuapp.com']
+ALLOWED_HOSTS = ['djangoratiba.herokuapp.com']
 #     # '127.0.0.1',
 #     # Add your domain name here if you have one
 # # ]
