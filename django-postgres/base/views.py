76598d1b75ecfd56645f08712cfb9e4a6a51208d
“BillyWaks” <136112711+BillyWaks@users.noreply.github.com>
2024-11-06 16:03:05 +0300

    views

--- django-postgres/base/views.py
+++ django-postgres/base/views.py
@@ -16,7 +16,7 @@ from rest_framework import generics
 
 class AuthenticatedAPIView(APIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]  # Changed to IsAuthenticated
+    permission_classes = [AllowAny]  # Changed to IsAuthenticated
 
 class EventList(AuthenticatedAPIView, generics.ListAPIView):
     """View to list all events."""
@@ -81,11 +81,14 @@ class ListParticipants(AuthenticatedAPIView, generics.ListAPIView):
     serializer_class = ParticipantSerializer
 
     def get_queryset(self):
-        # Use 'pk' instead of 'event_id'
-        event_id = self.kwargs['pk']
+        print(f"Kwargs: {self.kwargs}")  # Log kwargs to check if 'pk' is present
+        event_id = self.kwargs.get('pk')  # Safely access 'pk'
+        if not event_id:
+            print("No event_id found in kwargs")
         registration_objects = Registration.objects.filter(event_id=event_id)
         participants_ids = registration_objects.values_list('participant', flat=True)
         return Participant.objects.filter(id__in=participants_ids)
+
 
 class DeleteEvent(AuthenticatedAPIView, generics.DestroyAPIView):
     """View to delete an event."""
